869eea058b547c637bf1edc0ff97843ad1489f9f
laienwei <1250421964@qq.com>
2023-05-24 10:52:33 +0800

    for Dockerfile1

--- sixCloud.py
+++ sixCloud.py
@@ -61,6 +61,54 @@ for i in range(num):
     sheet.cell(row=p+i+1, column=4).value=sum0/(p+i-1)
 wb.save(filename3)
 wb.close()
+#####################################################################
+filedir3=['szseinnovation100indexmodel1cn.xlsx']
+dirba1="./lai/valuationquan/szseinnovation100index"
+filename3=dirba1+"/"+filedir3[0]
+wb = openpyxl.load_workbook(filename3)
+sheet = wb['myPEPB']
+p=sheet.max_row
+print(p)
+cell = sheet['C3']
+sum0=cell.value
+for i in range(p-3):
+    print(sum0)
+    sum0=sum0+sheet.cell(row=i+4, column=3).value
+print("over now")
+print(sum0)
+for i in range(num):
+    print(xx+i+1)
+    print(sheet0.cell(row=xx+i+1, column=22).value)
+    sum0=sum0+sheet0.cell(row=xx+i+1, column=22).value
+    sheet.cell(row=p+i+1, column=3).value=sheet0.cell(row=xx+i+1, column=22).value
+    sheet.cell(row=p+i+1, column=1).value=p+i-1
+    sheet.cell(row=p+i+1, column=2).value=lines[i]
+    sheet.cell(row=p+i+1, column=4).value=sum0/(p+i-1)
+wb.save(filename3)
+wb.close()
+
+filedir3=['CNIESG300indexmodel1cn.xlsx']
+dirba1="./lai/valuationquan/CNIESG300index"
+filename3=dirba1+"/"+filedir3[0]
+wb = openpyxl.load_workbook(filename3)
+sheet = wb['myPEPB']
+p=sheet.max_row
+print(p)
+cell = sheet['C3']
+sum0=cell.value
+for i in range(p-3):
+    print(sum0)
+    sum0=sum0+sheet.cell(row=i+4, column=3).value
+print("over now")
+print(sum0)
+for i in range(num):
+    sum0=sum0+sheet0.cell(row=xx+i+1, column=125).value
+    sheet.cell(row=p+i+1, column=3).value=sheet0.cell(row=xx+i+1, column=125).value
+    sheet.cell(row=p+i+1, column=1).value=p+i-1
+    sheet.cell(row=p+i+1, column=2).value=lines[i]
+    sheet.cell(row=p+i+1, column=4).value=sum0/(p+i-1)
+wb.save(filename3)
+wb.close()
 
 
 
