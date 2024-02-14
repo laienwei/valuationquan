5b85dc0b5cf213d08d1dfe0ebd14e94805521eef
laienwei <1250421964@qq.com>
2024-02-14 11:16:22 +0800

    for february

--- publai1.py
+++ publai1.py
@@ -1,7 +1,7 @@
 class publai1:
     def __init__(self):
-        filename='./data/datefortest83 - 副本.csv'
-        filename1='../data/datefortest83 - 副本.csv'
+        filename='./data/datefortest84 - 副本.csv'
+        filename1='../data/datefortest84 - 副本.csv'
         self.filename=filename
         self.filename1=filename1
     def __str__(self):
--- unify1ETF.py
+++ unify1ETF.py
@@ -7,7 +7,7 @@
 import openpyxl
 import xlrd
 from openpyxl.styles import Font,Alignment
-datelai='2023-12-29'
+datelai='2024-1-31'
 a=[]
 b=[]
 c=[]
