5f1a95be5ef9a9c34c5df4b649ed61f771b323c2
laienwei <1250421964@qq.com>
2024-04-02 11:52:56 +0800

    for april

--- publai1.py
+++ publai1.py
@@ -1,9 +1,10 @@
 class publai1:
     def __init__(self):
-        date='2024-3-29'
+        date0='2024-3-29'
         filename='./data/datefortest86 - 副本.csv'
         filename1='../data/datefortest86 - 副本.csv'
         self.filename=filename
         self.filename1=filename1
+        self.date=date0
     def __str__(self):
         return "子文件夹下的ipynb爬虫文件均使用变量filename1；根目录下的ipynb爬虫文件则使用变量filename。"
