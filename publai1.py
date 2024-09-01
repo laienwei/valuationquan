a86c1881bac8f131edf92a15f3c9dec0db474606
laienwei <1250421964@qq.com>
2024-09-01 14:13:31 +0800

    for september

--- publai1.py
+++ publai1.py
@@ -1,8 +1,9 @@
 class publai1:
     def __init__(self):
-        date0='2024-7-31'
-        filename='./data/datefortest90 - 副本.csv'
-        filename1='../data/datefortest90 - 副本.csv'
+        date0='2024-8-30'
+        num=91
+        filename='./data/datefortest'+str(num)+' - 副本.csv'
+        filename1='../data/datefortest'+str(num)+' - 副本.csv'
         self.filename=filename
         self.filename1=filename1
         self.date=date0
