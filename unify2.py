aa7a599a2f7531a501f5802fca5b71ca42f1ed2b
laienwei <1250421964@qq.com>
2025-04-03 10:44:00 +0800

    for new tag

--- unify2.py
+++ unify2.py
@@ -93,7 +93,7 @@ if (sheet2_data.cell_value(nrow-1,2)<=sheet2_data.cell_value(nrow-1,3)):
     amount=(sheet2_data.cell_value(nrow-1,3)-sheet2_data.cell_value(nrow-1,2)) * 3950
     biaozhi=1
 else:
-    amount=(sheet2_data.cell_value(nrow-1,3)-sheet2_data.cell_value(nrow-1,2)) * -3950
+    amount=(sheet2_data.cell_value(nrow-1,3)-sheet2_data.cell_value(nrow-1,2)) * 3950
     biaozhi=0
 sheet.cell(row=p+1, column=1).value=datelai
 sheet.cell(row=p+1, column=1).alignment=AlignmentText1
