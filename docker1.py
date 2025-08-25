2591743ab251ac46253c520fb89f0e279fca2889
laienwei <1250421964@qq.com>
2025-08-25 16:23:03 +0800

    for Dockerfile1

--- docker1.py
+++ docker1.py
@@ -38,11 +38,17 @@ wb = openpyxl.load_workbook(filename[3])
 c=list(range(p-3-num+1,p-3+1))
 del_ws_rows(wb,c,filename[3])
 wb.close()
+#####################################################
+os.system("cp " + ".cnb.yml " + ".cnb3.yml")
+os.system("mv " + ".cnb2.yml " + ".cnb.yml")
+os.system("mv " + ".cnb3.yml " + ".cnb2.yml")
 input()
 os.system("mv ../" + filename[0][-33:] +" " + filename[0][0:43])
 os.system("mv ../" + filename[1][-35:] +" " + filename[1][0:43])
 
 os.system("mv ../" + filename[2][-25:] +" " + filename[2][0:35])
 os.system("mv ../" + filename[3][-27:] +" " + filename[3][0:35])
-
-
+#####################################################
+os.system("cp " + ".cnb.yml " + ".cnb3.yml")
+os.system("mv " + ".cnb2.yml " + ".cnb.yml")
+os.system("mv " + ".cnb3.yml " + ".cnb2.yml")
