2975148e3bd2b569561ff8add2c58df45b33869b
laienwei <1250421964@qq.com>
2025-11-01 20:16:35 +0800

    for november

--- publai1.py
+++ publai1.py
@@ -1,8 +1,8 @@
 class publai1:
     def __init__(self):
-        date0='2025-9-30' 
+        date0='2025-10-31' 
         yearlai='2025' #for cloudnoVBAopenpyxl.py
-        num=104 #for sixCloud.py
+        num=105 #for sixCloud.py
         filename='./data/datefortest'+str(num)+' - 副本.csv'
         filename1='../data/datefortest'+str(num)+' - 副本.csv'
         self.filename=filename
