9e7ebf1c6fdb1fee887919d038e416243dcb851f
laienwei <1250421964@qq.com>
2026-01-08 21:07:11 +0800

    for january

--- publai1.py
+++ publai1.py
@@ -1,13 +1,18 @@
+
 class publai1:
     def __init__(self):
-        date0='2025-11-28' 
+        date0='2025-12-31' 
         yearlai='2025' #for cloudnoVBAopenpyxl.py
-        num=106 #for sixCloud.py
+        num,num1,num2=107,24,139 #for sixCloud.py
+        self.column399088=num1
+        self.column399378=num2
+        sheetname='start_from_2025.7.4' #sheetname of newworkbook
         filename='./data/datefortest'+str(num)+' - 副本.csv'
         filename1='../data/datefortest'+str(num)+' - 副本.csv'
         self.filename=filename
         self.filename1=filename1
         self.date=date0
         self.year0=yearlai
+        self.sheetname=sheetname
     def __str__(self):
         return "子文件夹下的ipynb爬虫文件均使用变量filename1；根目录下的ipynb爬虫文件则使用变量filename。"
--- sixCloud.py
+++ sixCloud.py
@@ -2,7 +2,6 @@
 # coding: utf-8
 
 # In[ ]:
-#在因为国证指数公司有新的指数发布后，399088和399378之前就有可能插入新的指数，本文件就需要更改，显然由于均值计算的缘故更改时需要用ctrl+H统一替换而绝非仅修改某一两个参数，切记切记！！！
 
 import openpyxl
 
@@ -12,7 +11,7 @@ with open(pub.filename,"r") as fp:
     lines=fp.readlines()
 num=len(lines)
 wb0 = openpyxl.load_workbook(r'./crawler/newworkbook.xlsx')
-sheet0 = wb0['start_from_2025.7.4']
+sheet0 = wb0[pub.sheetname]
 xx=sheet0.max_row-num #要读的数据从xx+1行开始
 filedir3=['szseinnovation100indexmodel1.xlsx']
 dirba1="./lai/valuationquan/szseinnovation100index"
@@ -30,10 +29,9 @@ print("The data summation process in the model1 files has ended.")
 print(sum0)
 for i in range(num):
     print(xx+i+1)
-    print(sheet0.cell(row=xx+i+1, column=24).value)
-    #sum0=sum0+float(sheet0.cell(row=xx+i+1, column=24).value)
-    sum0=sum0+sheet0.cell(row=xx+i+1, column=24).value
-    sheet.cell(row=p+i+1, column=3).value=sheet0.cell(row=xx+i+1, column=24).value
+    print(sheet0.cell(row=xx+i+1, column=pub.column399088).value)
+    sum0=sum0+sheet0.cell(row=xx+i+1, column=pub.column399088).value
+    sheet.cell(row=p+i+1, column=3).value=sheet0.cell(row=xx+i+1, column=pub.column399088).value
     sheet.cell(row=p+i+1, column=1).value=p+i-1
     sheet.cell(row=p+i+1, column=2).value=lines[i]
     sheet.cell(row=p+i+1, column=4).value=sum0/(p+i-1)
@@ -55,8 +53,8 @@ for i in range(p-3):
 print("over now")
 print(sum0)
 for i in range(num):
-    sum0=sum0+sheet0.cell(row=xx+i+1, column=139).value
-    sheet.cell(row=p+i+1, column=3).value=sheet0.cell(row=xx+i+1, column=139).value
+    sum0=sum0+sheet0.cell(row=xx+i+1, column=pub.column399378).value
+    sheet.cell(row=p+i+1, column=3).value=sheet0.cell(row=xx+i+1, column=pub.column399378).value
     sheet.cell(row=p+i+1, column=1).value=p+i-1
     sheet.cell(row=p+i+1, column=2).value=lines[i]
     sheet.cell(row=p+i+1, column=4).value=sum0/(p+i-1)
@@ -79,10 +77,9 @@ print("over now")
 print(sum0)
 for i in range(num):
     print(xx+i+1)
-    print(sheet0.cell(row=xx+i+1, column=24).value)
-    #sum0=sum0+float(sheet0.cell(row=xx+i+1, column=24).value)
-    sum0=sum0+sheet0.cell(row=xx+i+1, column=24).value
-    sheet.cell(row=p+i+1, column=3).value=sheet0.cell(row=xx+i+1, column=24).value
+    print(sheet0.cell(row=xx+i+1, column=pub.column399088).value)
+    sum0=sum0+sheet0.cell(row=xx+i+1, column=pub.column399088).value
+    sheet.cell(row=p+i+1, column=3).value=sheet0.cell(row=xx+i+1, column=pub.column399088).value
     sheet.cell(row=p+i+1, column=1).value=p+i-1
     sheet.cell(row=p+i+1, column=2).value=lines[i]
     sheet.cell(row=p+i+1, column=4).value=sum0/(p+i-1)
@@ -104,8 +101,8 @@ for i in range(p-3):
 print("over now")
 print(sum0)
 for i in range(num):
-    sum0=sum0+sheet0.cell(row=xx+i+1, column=139).value
-    sheet.cell(row=p+i+1, column=3).value=sheet0.cell(row=xx+i+1, column=139).value
+    sum0=sum0+sheet0.cell(row=xx+i+1, column=pub.column399378).value
+    sheet.cell(row=p+i+1, column=3).value=sheet0.cell(row=xx+i+1, column=pub.column399378).value
     sheet.cell(row=p+i+1, column=1).value=p+i-1
     sheet.cell(row=p+i+1, column=2).value=lines[i]
     sheet.cell(row=p+i+1, column=4).value=sum0/(p+i-1)
