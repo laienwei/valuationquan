1bc2298e4c9d51e40141edd60572361aab89aff0
laienwei <1250421964@qq.com>
2026-02-02 21:00:36 +0800

    for february

--- publai1.py
+++ publai1.py
@@ -6,13 +6,15 @@ class publai1:
         num,num1,num2=107,24,137 #for sixCloud.py
         self.column399088=num1
         self.column399378=num2
-        sheetname='start_from_2025.12.5' #sheetname of newworkbook
+        wbsheetname='start_from_2025.12.5' #sheetname of newworkbook
+        othersheetname='start_from_2024.12.6'
         filename='./data/datefortest'+str(num)+' - 副本.csv'
         filename1='../data/datefortest'+str(num)+' - 副本.csv'
         self.filename=filename
         self.filename1=filename1
         self.date=date0
         self.year0=yearlai
-        self.sheetname=sheetname
+        self.wbsheetname=wbsheetname
+        self.othersheetname=othersheetname
     def __str__(self):
         return "子文件夹下的ipynb爬虫文件均使用变量filename1；根目录下的ipynb爬虫文件则使用变量filename。"
