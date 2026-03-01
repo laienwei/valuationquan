c226ae3052d1d683e3f133e8debbbdba505401bf
laienwei <1250421964@qq.com>
2026-03-01 13:32:31 +0800

    for March

--- publai1.py
+++ publai1.py
@@ -1,9 +1,9 @@
 
 class publai1:
     def __init__(self):
-        date0='2025-12-31' 
-        yearlai='2025' #for cloudnoVBAopenpyxl.py
-        num,num1,num2=108,24,137 #for sixCloud.py
+        date0='2026-2-27' 
+        yearlai='2026' #for cloudnoVBAopenpyxl.py
+        num,num1,num2=109,24,137 #for sixCloud.py
         self.column399088=num1
         self.column399378=num2
         wbsheetname='start_from_2025.12.5' #sheetname of newworkbook
