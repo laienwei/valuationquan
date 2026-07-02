5713469670a4f3ac29f1a86961bcafc39f19f523
laienwei <1250421964@qq.com>
2026-07-02 19:08:56 +0800

    在publai1.py中留下注释中的中文history记录

--- publai1.py
+++ publai1.py
@@ -17,4 +17,5 @@ class publai1:
         self.wbsheetname=wbsheetname
         self.othersheetname=othersheetname
     def __str__(self):
+        # 2026年7月2日推送月底镜像上传CNB前，没有将实操镜像先推送上传到github，彻底完成后用git reset 返回之前状态cee853补上"for prac"的commit和git push，但效果不佳月底镜像中才有的文件立即出现在        # github中，所以github中的2026年6月底实操镜像是领先CNB的。最后又用git reset返回最新状态，并留下此段文字。
         return "子文件夹下的ipynb爬虫文件均使用变量filename1；根目录下的ipynb爬虫文件则使用变量filename。"
